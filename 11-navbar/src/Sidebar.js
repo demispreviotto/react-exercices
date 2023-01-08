2ad6a2476cbd410002b48092108688a9892429b3
Demis Previotto <demis.previotto@gmail.com>
2023-01-08 21:02:49 +0100

    Navbar commit

---
+++ 11-navbar/src/Sidebar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { links, social } from './data'
+
+const Sidebar = () => {
+    return (
+        <>
+            <div className="link-container show-container">
+                <ul className="links">
+                    {links.map((link) => {
+                        const { id, url, text } = link;
+                        return <li key={id}><a href={url}>{text}</a></li>
+                    })}
+                </ul>
+            </div>
+            <ul className="social-icons">
+                {social.map((socialIcon) => {
+                    const { id, url, icon, } = socialIcon;
+                    return (
+                        <li key={id}>
+                            <a href={url}>
+                                {icon}</a>
+                        </li>
+                    )
+                })}
+            </ul>
+
+        </>
+    )
+}
+
+export default Sidebar
