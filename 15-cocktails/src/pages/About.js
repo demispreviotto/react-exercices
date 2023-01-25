662c5f6ae06c7ca94ab613dc245a7b0e0a0d3b10
Demis Previotto <demis.previotto@gmail.com>
2023-01-25 20:17:50 +0100

    Cocktails commit

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci totam saepe accusantium quia, reprehenderit aut quis velit molestiae est fuga cum sit quidem suscipit eius? Omnis earum numquam sint quo?
+        Nisi dolorem sit eveniet necessitatibus exercitationem animi voluptatum repudiandae vitae eum nobis quod debitis, obcaecati fugiat voluptas? Assumenda reiciendis non voluptates sint voluptate. Molestias, est? Atque vitae odit dolorum dolor.
+        Sequi illo laudantium minima voluptate repellendus! Facere magnam illo temporibus, libero illum quasi nihil totam perspiciatis iusto mollitia nostrum animi ad praesentium exercitationem nobis culpa, officia harum cum rerum velit?
+        At iste ipsa consectetur itaque facere inventore animi fugit? Eligendi illo repellendus expedita neque, assumenda quo dolore vero commodi totam laboriosam suscipit aperiam repudiandae ad rerum mollitia voluptatem ab nulla.</p>
+    </section>
+  )
+}
+
+export default About
